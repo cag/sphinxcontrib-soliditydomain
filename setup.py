d9428e5345a9f677b86e81d9738f08de2889f2f6
Alan Lu <alanlu1023@gmail.com>
2018-05-11 17:45:25 -0500

    v0.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='sphinxcontrib-soliditydomain',
-    version='0.1.0',
+    version='0.2.0',
     description='Solidity domain for Sphinx',
 
     long_description=long_description,
